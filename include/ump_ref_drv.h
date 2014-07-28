91377c848a0f3455ff900ce8d309e287bb90bcdd
Javier Ferrer <javi.f.o@gmail.com>
2014-07-29 00:09:46 +0200

    exynos4: gralloc: update constant to match mali r4p0
    As defined here https://github.com/NamelessRom/android_kernel_samsung_smdk4412/blob/android-4.4/drivers/gpu/mali400/r4p0/ump/include/ump_uk_types.h#L57
    
    Change-Id: Ia4ccf15143a6575def528acef9a20e86710b18f4

--- include/ump_ref_drv.h
+++ include/ump_ref_drv.h
@@ -28,7 +28,7 @@ typedef enum {
     UMP_REF_DRV_CONSTRAINT_NONE = 0,
     UMP_REF_DRV_CONSTRAINT_PHYSICALLY_LINEAR = 1,
     /* This enum is included by samsung */
-    UMP_REF_DRV_CONSTRAINT_USE_CACHE = 128,
+    UMP_REF_DRV_CONSTRAINT_USE_CACHE = 4,
 } ump_alloc_constraints;
 
 /** Allocate an UMP handle containing a memory buffer.
